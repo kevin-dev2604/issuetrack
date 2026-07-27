53647f3f1c4d2d6527fc3247485d3d3b6071a180
kevin-dev2604 <kevin.j.dev.2604@gmail.com>
2026-07-27 19:26:58 +0900

    health check path 추가

---
+++ src/main/java/com/kevinj/portfolio/issuetrack/admin/adapter/in/web/HealthCheckController.java
@@ -0,0 +1,26 @@
+package com.kevinj.portfolio.issuetrack.admin.adapter.in.web;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class HealthCheckController {
+
+    @Operation(
+        summary = "Category Search",
+        description = "Admin account can search category information"
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "success"),
+    })
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+}
